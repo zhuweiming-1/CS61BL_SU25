1fe98b8da3af969839570c8e6a07e5b6681e94b9
karen <karenmeng06@gmail.com>
2025-06-24 00:04:43 -0700

    Updating lab02 skeleton

--- lab02/tests/ArrayOperationsTest.java
+++ lab02/tests/ArrayOperationsTest.java
@@ -1,73 +1,58 @@
-/** Unit tests for the ArrayOperations optional exercise. **/
+import org.junit.jupiter.api.*;
+
+import static com.google.common.truth.Truth.assertThat;
 public class ArrayOperationsTest {
 
-    public static boolean testInsert() {
+    @Test
+    public void testInsert() {
         int[] values = {1, 2, 3, 4, 5};
         ArrayOperations.insert(values, 0, 0);
         int[] afterInsert1 = {0, 1, 2, 3, 4};
-        if (!check(afterInsert1, values)) {
-            return false;
-        }
+        assertThat(check(afterInsert1, values)).isTrue();
 
         ArrayOperations.insert(values, 4, 7);
         int[] afterInsert2 = {0, 1, 2, 3, 7};
-        if (!check(afterInsert2, values)) {
-            return false;
-        }
-
-        return true;
+        assertThat(check(afterInsert2, values)).isTrue();
     }
 
-    public static boolean testDelete() {
+    @Test
+    public void testDelete() {
         int[] values = {1, 2, 3, 4, 5};
         ArrayOperations.delete(values, 4);
         int[] afterDelete1 = {1, 2, 3, 4, 0};
-        if (!check(afterDelete1, values)) {
-            return false;
-        }
+        assertThat(check(afterDelete1, values)).isTrue();
 
         values[4] = 5;
         ArrayOperations.delete(values, 2);
         int[] afterDelete2 = {1, 2, 4, 5, 0};
-        if (!check(afterDelete2, values)) {
-            return false;
-        }
+        assertThat(check(afterDelete2, values)).isTrue();
 
         values[4] = 7;
         ArrayOperations.delete(values, 0);
         int[] afterDelete3 = {2, 4, 5, 7, 0};
-        if (!check(afterDelete3, values)) {
-            return false;
-        }
-
-        return true;
+        assertThat(check(afterDelete3, values)).isTrue();
     }
 
-    public static boolean testCatenate() {
+    @Test
+    public void testCatenate() {
         int[] A = {1, 2, 3};
         int[] B = {4, 5};
         int[] firstValues = ArrayOperations.catenate(A, B);
         int[] afterCat1 = {1, 2, 3, 4, 5};
-        if (!check(afterCat1, firstValues)) {
-            return false;
-        }
+        assertThat(check(afterCat1, firstValues)).isTrue();
 
         int[] C = {4, 7, 9, 0};
         int[] D = {2, 4, 8, 9, 0, 9, 3};
         int[] secondValues = ArrayOperations.catenate(C, D);
         int[] afterCat2 = {4, 7, 9, 0, 2, 4, 8, 9, 0, 9, 3};
-        if (!check(afterCat2, secondValues)) {
-            return false;
-        }
-
-        return true;
+        assertThat(check(afterCat2, secondValues)).isTrue();
     }
 
     public static boolean check(int[][] expected, int[][] actual) {
         if (expected.length != actual.length) {
             System.out.format("Array length did not match expected length:\n"
-                    + "      Expected: %d\n"
-                    + "      Got: %d\n",
+                            + "      Expected: %d\n"
+                            + "      Got: %d\n",
                     expected.length, actual.length);
             return false;
         }
@@ -86,38 +71,20 @@ public class ArrayOperationsTest {
         }
         if (expected.length != actual.length) {
             System.out.format("Array length did not match expected length:\n"
-                    + "      Expected: %d\n"
-                    + "      Got: %d\n",
+                            + "      Expected: %d\n"
+                            + "      Got: %d\n",
                     expected.length, actual.length);
             return false;
         }
         for (int k = 0; k < expected.length; k++) {
             if (expected[k] != actual[k]) {
                 System.out.format("Array contents different at index %d:\n"
-                        + "    Expected: %d\n"
-                        + "    Got: %d\n",
+                                + "    Expected: %d\n"
+                                + "    Got: %d\n",
                         k, expected[k], actual[k]);
                 return false;
             }
         }
         return true;
-    }
-
-    public static void main(String[] args) {
-        if (testInsert()) {
-            System.out.println("testInsert passed!");
-        } else {
-            System.out.println("testInsert failed!");
-        }
-        if (testDelete()) {
-            System.out.println("testDelete passed!");
-        } else {
-            System.out.println("testDelete failed!");
-        }
-        if (testCatenate()) {
-            System.out.println("testCatenate passed!");
-        } else {
-            System.out.println("testCatenate failed!");
-        }
     }
 }
