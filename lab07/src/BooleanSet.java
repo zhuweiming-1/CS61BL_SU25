63125734dbb569421f2a6374c9ce5fa5aa64bd20
karen <karenmeng06@gmail.com>
2025-07-01 18:25:10 -0700

    Updating lab07 skeleton

---
+++ lab07/src/BooleanSet.java
@@ -0,0 +1,54 @@
+/**
+ * Represent a set of nonnegative ints from 0 to maxElement (inclusive) for some initially
+ * specified maxElement.
+ */
+public class BooleanSet implements SimpleSet {
+
+    private boolean[] contains;
+    private int size;
+
+    /** Initializes a set of ints from 0 to maxElement. */
+    public BooleanSet(int maxElement) {
+        contains = new boolean[maxElement + 1];
+        size = 0;
+    }
+
+    /** Adds k to the set. */
+    @Override
+    public void add(int k) {
+        // TODO: Implement this method.
+    }
+
+    /** Removes k from the set. */
+    @Override
+    public void remove(int k) {
+        // TODO: Implement this method.
+    }
+
+    /** Return true if k is in this set, false otherwise. */
+    @Override
+    public boolean contains(int k) {
+        return contains[k];
+    }
+
+
+    /** Return true if this set is empty, false otherwise. */
+    @Override
+    public boolean isEmpty() {
+        return this.size() == 0;
+    }
+
+    /** Returns the number of items in the set. */
+    @Override
+    public int size() {
+        // TODO: Implement this method.
+        return 0;
+    }
+
+    /** Returns an array containing all of the elements in this collection. */
+    @Override
+    public int[] toIntArray() {
+        // TODO: Implement this method.
+        return null;
+    }
+}
---
+++ lab07/test/CodingChallengesTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertWithMessage;
+
+public class CodingChallengesTest {
+
+    @Test
+    public void testMissingNumber() {
+	// TODO
+    }
+
+    @Test
+    public void testIsPermutation() {
+	// TODO
+    }
+}
