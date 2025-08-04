2446f8ed751dfcb75f8c8cc4a98f9bee3c90a08d
just two more labs to go - you're almost there! <karenmeng06@gmail.com>
2025-08-03 22:44:36 -0700

    Updating lab21 skeleton

---
+++ lab21/src/MergeSort.java
@@ -0,0 +1,33 @@
+public class MergeSort {
+
+
+    /**
+     * @param arr
+     *
+     * Sort the array arr using merge sort.
+     * The merge sort algorithm is as follows:
+     * 1. Split the collection to be sorted in half.
+     * 2. Recursively call merge sort on each half.
+     * 3. Merge the sorted half-lists.
+     *
+     */
+    public static int[] sort(int[] arr) {
+        // TODO: Implement merge sort
+        return arr;
+    }
+
+    /**
+     * @param a
+     * @param b
+     *
+     * Merge the sorted half-lists.
+     *
+     * Suggested helper method that will make it easier for you to implement merge sort.
+     */
+    private static int[] merge(int[] a, int[] b) {
+        int[] c = new int[a.length + b.length];
+        // TODO: Implement merge
+        return c;
+    }
+}
+
---
+++ lab21/tests/TestSorts.java
@@ -0,0 +1,70 @@
+import org.junit.jupiter.api.Test;
+import static com.google.common.truth.Truth.assertThat;
+
+
+
+public class TestSorts {
+
+    // TODO: Write more comprehensive tests that will help you debug your code.
+
+    @Test
+    public void testInsertionSort() {
+
+        int[] arr = {6, 3, 5, 7, 2, 1, 4};
+        int[] expected = {1, 2, 3, 4, 5, 6, 7};
+
+        InsertionSort.sort(arr);
+        assertThat(arr).isEqualTo(expected);
+
+
+    }
+
+    @Test
+    public void testSelectionSort() {
+
+        int[] arr = {6, 3, 5, 7, 2, 1, 4};
+        int[] expected = {1, 2, 3, 4, 5, 6, 7};
+
+        SelectionSort.sort(arr);
+        assertThat(arr).isEqualTo(expected);
+
+
+    }
+
+    @Test
+    public void testHeapSort() {
+
+        int[] arr = {6, 3, 5, 7, 2, 1, 4};
+        int[] expected = {1, 2, 3, 4, 5, 6, 7};
+
+        HeapSort.sort(arr);
+        assertThat(arr).isEqualTo(expected);
+
+
+    }
+
+    @Test
+    public void testMergeSort() {
+
+        int[] arr = {6, 3, 5, 7, 2, 1, 4};
+        int[] expected = {1, 2, 3, 4, 5, 6, 7};
+
+        int[] res = MergeSort.sort(arr);
+        assertThat(res).isEqualTo(expected);
+
+
+    }
+
+    @Test
+    public void testQuickSort() {
+
+        int[] arr = {6, 3, 5, 7, 2, 1, 4};
+        int[] expected = {1, 2, 3, 4, 5, 6, 7};
+
+        int[] res = QuickSort.sort(arr);
+        assertThat(res).isEqualTo(expected);
+
+
+    }
+
+}
